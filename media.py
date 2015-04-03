05f7fe2f42329aad370bacf5c1d8cdc7448fe93e
mahavasu <mahavasu@gmail.com>
2015-04-02 20:57:20 -0500

    Update media.py

--- media.py
+++ media.py
@@ -1,10 +1,13 @@
 
 import webbrowser
+
 class Movie():
     def __init__(self,movie_title,movie_storyline,movie_posterimage,movie_youtubetrailer):
+        #Creating class for movie that stores the information of that movie a
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = movie_posterimage
         self.trailer_youtube_url = movie_youtubetrailer
     def show_trailer(self):
+        #Function to play the trailer fo the given movie.
         webbrowser.open(self.youtube_trailer)
