db7539e534501988ab57d576222a1bf05d216d4b
mahavasu <mahavasu@gmail.com>
2015-04-06 08:44:36 -0500

    Update media.py

--- media.py
+++ media.py
@@ -1,5 +1,7 @@
-
+#import dependencies
 import webbrowser
+
+#Defining the class for a movie which has four objects tile of the movie,story line, its image,urlfor trailer
 
 class Movie():
     def __init__(self,movie_title,movie_storyline,movie_posterimage,movie_youtubetrailer):
@@ -8,6 +10,7 @@ class Movie():
         self.storyline = movie_storyline
         self.poster_image_url = movie_posterimage
         self.trailer_youtube_url = movie_youtubetrailer
+        
     def show_trailer(self):
-        #Function to play the trailer fo the given movie.
-        webbrowser.open(self.youtube_trailer)
+        #method Function to play the trailer for the given movie.
+        webbrowser.open(self.youtube_trailer)# this is a method for the class movie
